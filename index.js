96aa4f92e164c73a550af1bd58ccf24fb7a9055a
Rodrigocleyton <88049769+Rodrigocleyton@users.noreply.github.com>
2023-01-07 14:31:53 -0300

    DataBase connection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 const bodyParser = require('body-parser')
+const connetion = require('./dataBase/database')
 
 //view engine
 app.set('view engine', 'ejs')
@@ -10,8 +11,17 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 //body parser
-app.use(bodyParser.urlencoded({extends:false}))
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+//DataBase
+connetion 
+    .authenticate()
+    .then(()=>{
+        console.log("DataBase connectio sucess")
+    }).catch((error)=>{
+        console.log("Not DaraBase connection", error)
+    })
 
 app.get("/", (req,res) =>{
     res.render("index")
