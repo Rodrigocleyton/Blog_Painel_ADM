24f5cee40c33c62a07b96a2807cfac14df3aa71c
Rodrigocleyton <88049769+Rodrigocleyton@users.noreply.github.com>
2023-01-06 21:10:41 -0300

    Views config

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const bodyParser = require('body-parser')
+
+//view engine
+app.set('view engine', 'ejs')
+
+//body parser
+app.use(bodyParser.urlencoded({extends:false}))
+app.use(bodyParser.json())
 
 app.get("/", (req,res) =>{
-    res.send("Bem vindo ao meu site!")
+    res.render("index")
 })
 
 app.listen(port, ()=>{
