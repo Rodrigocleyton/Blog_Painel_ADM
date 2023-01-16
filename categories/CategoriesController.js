389f860ae99a83361e634a5c110fba236ea6ad3e
Rodrigocleyton <88049769+Rodrigocleyton@users.noreply.github.com>
2023-01-15 21:13:43 -0300

    Cadastro de categorias no BD

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,25 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req,res)=>{
     res.render("admin/categories/new")
 })
+
+router.post("/categories/save", (req, res) =>{
+    var title = req.body.title
+    if(title !=undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+            //otimiza a instirng para url para que seja utilizado diretamente na rota
+        }).then(()=>{
+            res.redirect("/")
+        })
+    } else {
+        res.redirect("/admin/categorias/new")
+    }
+
+});
 module.exports = router
